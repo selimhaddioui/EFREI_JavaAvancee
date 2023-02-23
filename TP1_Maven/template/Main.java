1ad829d9f7510609eec2c8392aa644608d3b9a28
Selim Haddioui <selim.haddioui@hotmail.com>
2023-02-23 07:49:09 +0100

    adding code template

---
+++ TP1_Maven/template/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        double val1 = 2, val2 = 3, sum, product;
+        sum = calculation.sum(val1, val2);
+        product = calculation.mult(val1, val2);
+        System.out.println("Hello world!");
+        System.out.println(val1 + " + " + val2 + " = " + sum);
+        System.out.println(val1 + " * " + val2 + " = " + product);
+    }
+}
