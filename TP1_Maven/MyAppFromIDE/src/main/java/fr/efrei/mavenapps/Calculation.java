44bf3617397d1f3afcbf3f97210216d6eea4eb5b
NGUYEN <minh-tri.nguyen@efrei.net>
2023-02-28 08:53:15 +0100

    exercice2

---
+++ TP1_Maven/MyAppFromIDE/src/main/java/fr/efrei/mavenapps/Calculation.java
@@ -0,0 +1,11 @@
+package fr.efrei.mavenapps;
+
+public class Calculation {
+    double sum(double val1, double val2){
+        return Double.sum(val1, val2);
+    }
+
+    double mult(double val1, double val2){
+        return val1 * val2;
+    }
+}
---
+++ TP1_Maven/MyAppFromMaven/src/test/java/fr/efrei/mavenapps/CalculationTest.java
@@ -0,0 +1,30 @@
+package fr.efrei.mavenapps;
+
+import junit.framework.TestCase;
+
+public class CalculationTest extends TestCase {
+
+    public void testSum() {
+        // Arrange
+        Calculation calculation = new Calculation();
+        double val1 = 2.0, val2 = 3.0, expected = 5.0, actual;
+
+        // Act
+        actual = calculation.sum(val1, val2);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    public void testMult() {
+        // Arrange
+        Calculation calculation = new Calculation();
+        double val1 = 2.0, val2 = 3.0, expected = 6.0, actual;
+
+        // Act
+        actual = calculation.mult(val1, val2);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+}
